dc8b568ef8134c3363ac526941e5ac150e8a96a8
sulegebedek <sulegebedek@gmail.com>
2021-04-17 03:15:25 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kayıt başarıyla gerçekleştirildi.";
+        public static string UserAlreadyExists = "Böyle bir kullanıcı kaydı mevcut.";
+        public static string AccessTokenCreated = "Giriş yapıldı";
+        public static string UserNotFound = "Böyle bir kullanıcı bulunamadı.";
+        public static string PasswordError = "Geçersiz parola";
+        public static string SuccessfulLogin = "Bşarılı Giriş";
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/FileShareDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class FileShareDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=FileShare;Trusted_Connection=true");
+        }
+    }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+}
